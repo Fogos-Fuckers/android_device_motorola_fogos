2f52afe91b7a0ae25e33d3f778c5497abd454462
Michael Bestas <mkbestas@lineageos.org>
2025-05-23 00:53:41 +0300

    fogos: Update to V1UG35H.75-14-9
    Change-Id: Iaa6175134bcb9ea6bbed5063a371bf720e82124e

--- lineage_fogos.mk
+++ lineage_fogos.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := moto g34 5G
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="fogos_g-user 15 V1UG35H.75-14-3 6c77f4-ac72a9 release-keys MV-186" \
-    BuildFingerprint=motorola/fogos_g/fogos:15/V1UG35H.75-14-3/6c77f4-ac72a9:user/release-keys \
+    BuildDesc="fogos_g-user 15 V1UG35H.75-14-9 abc528-11988f release-keys MV-186" \
+    BuildFingerprint=motorola/fogos_g/fogos:15/V1UG35H.75-14-9/abc528-11988f:user/release-keys \
     DeviceProduct=fogos_g
